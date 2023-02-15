32bd3df5c72bb50cbf822af47b5f3bc05c1d2fe2
Ebrahim Nasser <ebrahimm.nasser@gmail.com>
2023-02-15 06:14:16 +0200

    edit file

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import TextSphere from "./TextSphere/TextSphere";
+import TextSphere from "./TextSphere/TextSphere.jsx";
 
 const Skills = ({ sections }) => {
   return (
